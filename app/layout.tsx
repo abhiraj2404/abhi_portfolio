0d9fe7825be66cc8c249f6c50f51ac267439402e
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2024-12-25 23:06:45 +0530

    feat:added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from "./providers";
 import Navigation from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navigation />
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
