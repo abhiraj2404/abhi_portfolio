d3de89389b7d354f19e067b54337548ad570e364
Abhiraj Singh Chauhan <135122367+abhiraj2404@users.noreply.github.com>
2026-07-27 02:20:39 +0530

    Update CV download link in TypeWriter component

--- components/ui/TypeWriter.tsx
+++ components/ui/TypeWriter.tsx
@@ -16,7 +16,7 @@ export function Typewriter() {
     <div className="flex flex-col items-center justify-center h-[10rem]">
       <TypewriterEffectSmooth words={words} className="text-white" />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <Link href="https://drive.google.com/file/d/10rowI5--E_BunJQcAcID5YndwCGdEG2B/view?usp=sharing">
+        <Link href="https://drive.google.com/file/d/1LoxRyD74tYNYsHU1Gii9-axj6IN4pC_Q/view?usp=sharing">
           <button className="w-40 h-10 rounded-xl bg-blue-500 hover:bg-blue-900 dark:border-white border-white text-white text-sm">
             Download CV
           </button>
